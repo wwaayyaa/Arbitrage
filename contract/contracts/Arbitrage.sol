c5b17be874ca07ec9d38e734a0a3a82530052671
Yang Wang <357466524@qq.com>
2020-12-24 16:47:41 +0800

    wip

--- contract/contracts/Arbitrage.sol
+++ contract/contracts/Arbitrage.sol
@@ -10,13 +10,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./utils/Withdrawable.sol";
 
 contract Arbitrage is Withdrawable {
+    using SafeERC20 for IERC20;
     event StepTest(string n1, string n2, string n3, string n4);
-
-    function a2test(string memory arg1, string memory arg2, string memory arg3, string memory arg4, string memory arg5, string memory arg6, string memory arg7, string memory arg8) public {
-        emit StepTest(arg1, arg2, arg3, arg4);
-        emit StepTest(arg5, arg6, arg7, arg8);
-        require(compareStrings(arg1, arg2), "heiheihei not ==");
-    }
 
     //    function tradeUniswap(address router, uint amountIn, address path1, address path2){
     //
@@ -27,9 +22,39 @@ contract Arbitrage is Withdrawable {
     //    }
     address[] path;
 
-    //协议、ex（uniswap传入router地址，balancer传入pool地址）、addrfrom、addrto、amount
-    function a2(string memory protocol1, address ex1, address addr11, address addr12, uint amount1,
-        string memory protocol2, address ex2, address addr21, address addr22, uint amount2) public {
+    modifier ensure(uint height) {
+        require(height >= block.number, 'error: LATE');
+//        require(deadline >= block.timestamp, 'error: EXPIRED');
+        _;
+    }
+
+    function stepExecutor(string memory n, uint8 protocol, address ex, address addr1, address addr2, uint amount, uint minAmountOut, uint timestamp) internal returns (uint){
+        bool ok = IERC20(addr1).approve(ex, amount);
+        require(ok, append("approve error", n));
+
+        uint balanceBefore = IERC20(addr2).balanceOf(address(this));
+
+        if (protocol == 1) {
+            delete path;
+            path.push(addr1);
+            path.push(addr2);
+            IUniswapV2Router01(ex).swapExactTokensForTokens(amount, minAmountOut, path, address(this), timestamp);
+        } else if (protocol == 2) {
+            IBPool(ex).swapExactAmountIn(addr1, amount, addr2, minAmountOut, 999999999999999999999999999999999999999999999);
+        } else {
+            require(false, append("unknown p", n));
+        }
+        uint balanceAfter = IERC20(addr2).balanceOf(address(this));
+        require(balanceAfter > balanceBefore, append("error ", n));
+
+        return balanceAfter - balanceBefore;
+    }
+
+    //协议(1 uniswap, 2 balancer)、ex（uniswap传入router地址，balancer传入pool地址）、addrfrom、addrto、amount
+    function a2(uint height,
+        uint8 protocol1, address ex1, address addr11, address addr12, uint amount1, uint minAmountOut1,
+        uint8 protocol2, address ex2, address addr21, address addr22, uint amount2, uint minAmountOut2
+    ) public ensure(height) {
         //根据类型1判断交易所
         //需要approve权限
         //调用swap方法
@@ -40,84 +65,43 @@ contract Arbitrage is Withdrawable {
 
         uint wethBalanceBefore = IERC20(addr11).balanceOf(address(this));
         //step 1
-        uint balanceBefore = 0;
-        uint balanceAfter = 0;
-        if (compareStrings(protocol1, "uniswap")) {
-            bool ok = IERC20(addr11).approve(ex1, amount1);
-            require(ok, "approve1 error");
-            delete path;
-            path.push(addr11);
-            path.push(addr12);
-            balanceBefore = IERC20(addr12).balanceOf(address(this));
-            IUniswapV2Router01(ex1).swapExactTokensForTokens(amount1, 0, path, address(this), 3333333333);
-            balanceAfter = IERC20(addr12).balanceOf(address(this));
-            require(balanceAfter > balanceBefore, "error 1");
-        } else if (compareStrings(protocol1, "balancer")) {
-            bool ok = IERC20(addr11).approve(ex1, amount1);
-            require(ok, "approve1 error");
-            balanceBefore = IERC20(addr12).balanceOf(address(this));
-            IBPool(ex1).swapExactAmountIn(addr11, amount1, addr12, 0, 999999999999999999999999999999999999999999999);
-            balanceAfter = IERC20(addr12).balanceOf(address(this));
-            require(balanceAfter > balanceBefore, "error 1");
-        } else {
-            require(false, "unknown p1");
-        }
+        uint balance = stepExecutor("1", protocol1, ex1, addr11, addr12, amount1, minAmountOut1, 3333333333);
         //step 2
-        uint balance = balanceAfter - balanceBefore;
-        if (compareStrings(protocol2, "uniswap")) {
-            bool ok = IERC20(addr21).approve(ex2, balance);
-            require(ok, "approve1 error");
-            delete path;
-            path.push(addr21);
-            path.push(addr22);
-            balanceBefore = IERC20(addr22).balanceOf(address(this));
-            IUniswapV2Router01(ex2).swapExactTokensForTokens(balance, 0, path, address(this), 3333333333);
-            balanceAfter = IERC20(addr22).balanceOf(address(this));
-            require(balanceAfter > balanceBefore, string(abi.encodePacked("error 2 ", uint2str(balanceAfter), " ", uint2str(balanceBefore))));
-        } else if (compareStrings(protocol2, "balancer")) {
-            bool ok = IERC20(addr21).approve(ex2, balance);
-            require(ok, "approve1 error");
-            balanceBefore = IERC20(addr22).balanceOf(address(this));
-            IBPool(ex2).swapExactAmountIn(addr21, balance, addr22, 0, 999999999999999999999999999999999999999999999);
-            balanceAfter = IERC20(addr22).balanceOf(address(this));
-            require(balanceAfter > balanceBefore, string(abi.encodePacked("error 2 ", uint2str(balanceAfter), " ", uint2str(balanceBefore))));
-        } else {
-            require(false, "unknown p2");
-        }
+        stepExecutor("2", protocol2, ex2, addr21, addr22, balance, minAmountOut2, 3333333333);
+
         uint wethBalanceAfter = IERC20(addr11).balanceOf(address(this));
-        require(wethBalanceAfter > wethBalanceBefore, string(abi.encodePacked("error 3 ", uint2str(wethBalanceAfter), " ", uint2str(wethBalanceBefore))));
+
+        require(wethBalanceAfter > wethBalanceBefore, string(abi.encodePacked("error finish ")));
     }
 
-    function compareStrings(string memory a, string memory b) public view returns (bool) {
-        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
-    }
+    function a3(uint height,
+        uint8 protocol1, address ex1, address addr11, address addr12, uint amount1, uint minAmountOut1,
+        uint8 protocol2, address ex2, address addr21, address addr22, uint amount2, uint minAmountOut2,
+        uint8 protocol3, address ex3, address addr31, address addr32, uint amount3, uint minAmountOut3
+    ) public ensure(height) {
+        uint wethBalanceBefore = IERC20(addr11).balanceOf(address(this));
+        //step 1
+        uint balance = stepExecutor("1", protocol1, ex1, addr11, addr12, amount1, minAmountOut1, 3333333333);
+        //step 2
+        balance = stepExecutor("2", protocol2, ex2, addr21, addr22, balance, minAmountOut2, 3333333333);
+        //step 3
+        stepExecutor("3", protocol3, ex3, addr31, addr32, balance, minAmountOut3, 3333333333);
 
-    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
-        if (_i == 0) {
-            return "0";
-        }
-        uint j = _i;
-        uint len;
-        while (j != 0) {
-            len++;
-            j /= 10;
-        }
-        bytes memory bstr = new bytes(len);
-        uint k = len - 1;
-        while (_i != 0) {
-            bstr[k--] = byte(uint8(48 + _i % 10));
-            _i /= 10;
-        }
-        return string(bstr);
+        uint wethBalanceAfter = IERC20(addr11).balanceOf(address(this));
+
+        require(wethBalanceAfter > wethBalanceBefore, string(abi.encodePacked("error finish ")));
     }
 
     function withdrawN(address _assetAddress, uint amount) public onlyOwner {
         if (_assetAddress == ETHER) {
             msg.sender.transfer(amount);
         } else {
-            ERC20(_assetAddress).safeTransfer(msg.sender, amount);
+            IERC20(_assetAddress).safeTransfer(msg.sender, amount);
         }
         emit LogWithdraw(msg.sender, _assetAddress, amount);
     }
 
+    function append(string memory a, string memory b) internal pure returns (string memory) {
+        return string(abi.encodePacked(a, b));
+    }
 }
